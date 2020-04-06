b7a6aa8e9ef6e03d9f1e6bcbef72ef15efff3745
zyq_1015 <3074995186@qq.com>
2020-04-06 18:30:46 +0800

    提交代碼2020年4月6日18:30:35李浩

--- garbage-collection-service/src/main/java/com/dong/garbage/controller/demo.java
+++ garbage-collection-service/src/main/java/com/dong/garbage/controller/demo.java
@@ -1,9 +1,22 @@
 package com.dong.garbage.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class demo {
 
-    public void test(){
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
 
+
+    @PostMapping("aaaaaaa")
+    public void test(){
+        redisTemplate.opsForValue().set("aaa","aaaa");
+        String aaa = redisTemplate.opsForValue().get("aaa");
+        System.out.println(aaa);
     }
     public void demoOne(){
 
