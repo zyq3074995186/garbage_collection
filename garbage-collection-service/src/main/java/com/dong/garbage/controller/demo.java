9e6ab2596abfdb7ff4e244a783d149e1aff148ba
taoyanjun <taoyanjun@ah-fuli.cn>
2020-04-06 18:31:07 +0800

    更新代码

--- garbage-collection-service/src/main/java/com/dong/garbage/controller/demo.java
+++ garbage-collection-service/src/main/java/com/dong/garbage/controller/demo.java
@@ -1,7 +1,14 @@
 package com.dong.garbage.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("testDemo")
 public class demo {
 
+	@PostMapping("test")
     public void test(){
 
     }
