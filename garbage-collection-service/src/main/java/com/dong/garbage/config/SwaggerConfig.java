6bee9560b107bbcbcb2ce5482121e84e922052e6
zyq_1015 <3074995186@qq.com>
2020-04-13 10:43:35 +0800

    提交代碼2020年4月6日18:30:35李浩

--- garbage-collection-service/src/main/java/com/dong/garbage/config/SwaggerConfig.java
+++ garbage-collection-service/src/main/java/com/dong/garbage/config/SwaggerConfig.java
@@ -6,7 +6,6 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,23 +19,18 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                //为当前包路径
-                .apis(RequestHandlerSelectors.basePackage("com.dong.garbage.config"))
+                .apis(RequestHandlerSelectors.basePackage("com.dong.garbage.controller"))
+                //扫描文档注解的包路径
                 .paths(PathSelectors.any())
                 .build();
-//        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build();
     }
-    //构建 api文档的详细信息函数,注意这里的注解引用的是哪个
+
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                //页面标题
-                .title("Spring Boot 使用 Swagger2 构建RESTful API")
-                //创建人
-                .contact(new Contact("","",""))
-                //版本号
+                .title("测试")
+                //标题
+                .description("Create by Alan Di")
                 .version("1.0")
-                //描述
-                .description("API 描述")
                 .build();
     }
 
--- garbage-collection-service/src/main/java/com/dong/garbage/controller/demo.java
+++ garbage-collection-service/src/main/java/com/dong/garbage/controller/demo.java
@@ -1,6 +1,7 @@
 package com.dong.garbage.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ public class demo {
     private RedisTemplate<String, String> redisTemplate;
 
 
+    @ApiOperation("测试接口")
     @PostMapping("aaaaaaa")
     public void test(){
         redisTemplate.opsForValue().set("aaa","aaaa");
